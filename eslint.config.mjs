d7ba7e5c350c785cd6293342c439c584c91e8f2d
Ferdinand Thiessen <opensource@fthiessen.de>
2026-07-06 13:20:59 +0200

    chore: migrate to nextcloud-eslint config
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,10 +3,8 @@
  * SPDX-License-Identifier: MIT
  */
 
-import eslint from '@eslint/js'
+import { recommendedLibrary } from '@nextcloud/eslint-config'
 import { defineConfig } from 'eslint/config'
 import tseslint from 'typescript-eslint'
 
-export default defineConfig(eslint.configs.recommended, tseslint.configs.recommended, {
-	ignores: ['lib/**', 'dist/**', 'node_modules/**', 'coverage/**'],
-})
+export default defineConfig([...recommendedLibrary])
